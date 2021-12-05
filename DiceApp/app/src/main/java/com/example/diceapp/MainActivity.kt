29ea89139931389b195af7bffe4c25b7b245e2c5
alejorminetti <alejorm@gmail.com>
2021-12-05 10:27:33 -0300

    Button that display a Toast

---
+++ DiceApp/app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.diceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+}
