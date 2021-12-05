f3ad5cabfec528c6c318619735899790859618df
alejorminetti <alejorm@gmail.com>
2021-12-05 11:32:37 -0300

    Adding comments and Toast message when same number is rolled

--- DiceApp/app/src/main/java/com/example/diceapp/MainActivity.kt
+++ DiceApp/app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -6,27 +6,51 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private var lastRoll: Int? = null
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+
+        // additional functionality: if roll same number again, send a Toast
+        if ( diceRoll == lastRoll){
+            Toast.makeText(this, "$diceRoll rolled again!", Toast.LENGTH_SHORT).show()
+        }
+        lastRoll = diceRoll
+
+        // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
 }
 
+/**
+ * This is the Dice class
+ * atribute:
+ * numSide: the number of sides of the dice
+ * method:
+ * roll(): generate random number from 1 to numSide
+ */
 class Dice(private val numSides: Int) {
-
+    /**
+     * generate random number from 1 to numSide
+     */
     fun roll(): Int {
         return (1..numSides).random()
     }
