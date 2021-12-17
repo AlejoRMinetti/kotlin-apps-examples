5d2fdbf0a97b9ea5583f0217b8e5eb7e17fd5398
alejorminetti <alejorm@gmail.com>
2021-12-17 01:11:54 -0300

    Dice Image when you roll!

--- DiceApp/app/src/main/java/com/example/diceapp/MainActivity.kt
+++ DiceApp/app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -24,16 +25,19 @@ class MainActivity : AppCompatActivity() {
      */
     private fun rollDice() {
         // Create 2 new Dices object with 6 sides and roll it
-        val dice1 = Dice(6)
-        val dice1Roll = dice1.roll()
-        val dice2 = Dice(6)
-        val dice2Roll = dice2.roll()
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
 
-        // Update the screen with the dice rolls
-        val result1TextView: TextView = findViewById(R.id.dice1_textView)
-        result1TextView.text = dice1Roll.toString()
-        val result2TextView: TextView = findViewById(R.id.dice2_textView)
-        result2TextView.text = dice2Roll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
 
