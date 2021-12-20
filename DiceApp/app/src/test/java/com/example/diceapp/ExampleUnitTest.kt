396c6b8d3fae57fa73f9c50b499c821a1529e190
Alejo R Minetti <alejorm@gmail.com>
2021-12-20 08:09:32 -0300

    Running junit Test for the Dice range based on its faces

--- DiceApp/app/src/test/java/com/example/diceapp/ExampleUnitTest.kt
+++ DiceApp/app/src/test/java/com/example/diceapp/ExampleUnitTest.kt
@@ -10,8 +10,17 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        for ( i in 1..100){
+        val dice = Dice(7)
+        val rollResult = dice.roll()
+        println("rollResult: ${rollResult}   attempt: ${i}")
+        assertTrue( "The value of rollResult was not between 1 and 6",
+            rollResult in 1..6
+        )
+        }
     }
+
 }
